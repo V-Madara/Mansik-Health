128d0c9813b73cb6999571e4d437449e901b848c
Vishal Prajapati <vishalkumarprajapati1881@gmail.com>
2026-09-22 07:37:42 +0530

    Final Deployment

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
    submission to FastAPI /predict, results rendering, print/reset.
    ========================================================================== */
 
-const API_URL = 'http://127.0.0.1:8000';
+const API_URL = 'https://mansik-health-score.onrender.com/';
 
 /* -------------------------------------------------------------------------
    Header / navigation
